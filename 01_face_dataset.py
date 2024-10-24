25f105e5450a92360b57b0d04121d4154952142d
KTo1 <kto@lovetou.ru>
2024-10-24 15:23:40 +0700

    first commit

--- 01_face_dataset.py
+++ 01_face_dataset.py
@@ -29,7 +29,7 @@ while (True):
     ret, img = cam.read()
     #img = cv2.flip(img, -1)  # flip video image vertically
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_detector.detectMultiScale(gray, 1.3, 5)
+    faces = face_detector.detectMultiScale(gray, 1.3, 6)
 
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
@@ -43,7 +43,7 @@ while (True):
     k = cv2.waitKey(100) & 0xff  # Press 'ESC' for exiting video
     if k == 27:
         break
-    elif count >= 130:  # Take 30 face sample and stop video
+    elif count >= 30:  # Take 30 face sample and stop video
         break
 
 # Do a bit of cleanup
